2e330d76ff27a5a8d55b7ececa9e7062fc16982f
Dominik277 <dominikdoric277@gmail.com>
2020-12-14 19:42:50 +0100

    Dodan interfejs TaskDao koji nam predstavlja Dao.U sebi sadrzi metode za insert, delete i update baze podataka.

---
+++ app/src/main/java/room/database/TaskDao.java
@@ -0,0 +1,25 @@
+package room.database;
+
+import java.util.List;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+@Dao
+public interface TaskDao {
+
+    @Query("SELECT * FROM task")
+    List<Task> getAll();
+
+    @Insert
+    void insert(Task task);
+
+    @Delete
+    void delete(Task task);
+
+    @Update
+    void update(Task task);
+
+}
