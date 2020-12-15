cfb3f2889fa02290552bd71e6523237f4c112dfe
Dominik277 <dominikdoric277@gmail.com>
2020-12-15 17:30:04 +0100

    Dodane sve reference preko kojih cemo dohvacati View-ove.

--- app/src/main/java/room/database/AddTastActivity.java
+++ app/src/main/java/room/database/AddTastActivity.java
@@ -1,4 +1,34 @@
 package room.database;
 
-public class AddTastActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddTastActivity extends AppCompatActivity {
+
+    private EditText editTextTask;
+    private EditText editTextDesc;
+    private EditText editTextFinishBy;
+    private Button button_save;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        editTextTask = findViewById(R.id.editTextTask);
+        editTextDesc = findViewById(R.id.editTextDesc);
+        editTextFinishBy = findViewById(R.id.editTextFinishBy);
+        button_save = findViewById(R.id.button_save);
+
+        button_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveTask();
+            }
+        });
+
+    }
 }
