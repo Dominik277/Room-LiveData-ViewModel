76ebb39d056c9519b02725f97d9ea958724a31a2
Dominik277 <dominikdoric277@gmail.com>
2020-12-16 18:13:48 +0100

    Stvorene reference preko kojih cemo dohvacati atributa iz XML-a i ispravljen dio unutar recyclerview_tasks.xml te implementirana onClick() metoda.

--- app/src/main/java/room/database/TasksAdapter.java
+++ app/src/main/java/room/database/TasksAdapter.java
@@ -1,13 +1,20 @@
 package room.database;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class TasksAdapter extends RecyclerView.Adapter<TasksAdapter.TasksViewHolder> {
 
+    private Context mCtx;
+    private List<Task> taskList;
 
     @NonNull
     @Override
@@ -27,12 +34,29 @@ public class TasksAdapter extends RecyclerView.Adapter<TasksAdapter.TasksViewHol
 
     class TasksViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
+        TextView textViewStatus;
+        TextView textViewTask;
+        TextView textViewDesc;
+        TextView textViewFinishBy;
+
         public TasksViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            textViewStatus = itemView.findViewById(R.id.textViewStatus);
+            textViewTask = itemView.findViewById(R.id.textViewTask);
+            textViewDesc = itemView.findViewById(R.id.textViewDesc);
+            textViewStatus = itemView.findViewById(R.id.textViewStatus);
+
+            itemView.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View v) {
+
+            Task task = taskList.get(getAdapterPosition());
+
+            Intent intent = new Intent(mCtx, UpdateTaskActivity.class);
+            intent.putExtra("task",task);
 
         }
     }
