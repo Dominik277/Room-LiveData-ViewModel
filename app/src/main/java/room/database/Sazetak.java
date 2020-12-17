0f63bcc47ef623d211c92fa59f36491c1f570b08
Dominik277 <dominikdoric277@gmail.com>
2020-12-17 19:19:37 +0100

    Dodano objasnjenja za AddTastActivity.

--- app/src/main/java/room/database/Sazetak.java
+++ app/src/main/java/room/database/Sazetak.java
@@ -7,10 +7,31 @@ MainMainActivity
 ------------------
 AddTastActivity
 ------------------
+-Osim Main aktivnosti koja se prikazuje pri pokretanju aplikacije kao prvi prozor mi također imamo jos dvije aktivnosti, a to su
+ AddTastActivity i UpdateTaskActivity.Ova klasa nam predstavlja logiku za aktivnost AddTastActivity.U XML-u smo definirali tocno
+ kako ce izgledati ekran kada se otvori ova aktivnost, a ona ce izgledati tako da ce gore pisati naslov "Add a Task" a ispod tog
+ naslova ce biti tri EditText-a u koje korisnik upisuje zadatk,opis zadatka i do kad bi trebao ispuniti taj zadatak, te jos na
+ kraju imamo i gumb pomocu kojeg se te informacije pohranjuju u bazu podataka.Mi nakon sto smo definirali vizualni dio u XML-u
+ jos uvijek ne mozemo nista jer nismo implementirali nikakvu logiku, a upravo to se pravi unutar ove klase.Kao pocetak ove klase
+ prvo deklariramo imena preko kojih cemo referencirati te View-ove(tri EditText-a i jedan Button).Nakon toga imenima tih varijabli
+ dodjelimo te View-e iz XML-a pomocu metode findViewById().Nakon toga navodimo sta ce se desiti kada kliknemo misem na gumb,definirali
+ smo da ce se prilikom klika na gumb izvesti metoda saveTask() koju cemo deklarirati u sljedecem koraku.saveTask() metoda nam sluzi
+ kako bi ono sto je korisnik unio u polja EditText-a pohranili u bazu podataka.Na pocetku ove metode tekst koji se unio u svaki
+ EditText se pohranjuje u odgovarajuce varijable(sTask,sDesc,sFinishBy) pomocu metoda getText() i toString().Sada kada imamo tekst
+ pohranjen unutar varijabli onda mozemo izvrsavati i kontrolu, u nasem slucaju ako korisnik nije nista unio u polje EditText-a onda
+ se na kraju polja prikazuje mali crveni usklicnik sa popup porukom da nije unio nikakav tekst a stisnuo je dalje i tako za sva tri
+ polja.Poslije toga se obavlja glavni zadatak u ovoj klasi, a to je unosenje podataka koje je korisnik unio u bazu podataka.To se radi
+ u nested(inner) klasi koja nasljeđuje AsyncTask klasu,a ova klasa nam sluzi za obavljanje teskih zadataka u zasebnom thread-u.Ako bi
+ proces spremanja i povlacenja podataka iz baze podataka radili na main UI thread-u onda bi to prouzrocilo aplikaciju da se crasha jer
+ bi main UI thread bio preopterecen, pa se zbog toga ovakvi teski zadati obavljaju u backgroundu pomocu metode doInBackground() i onda
+ se rezultati koji se dobiju u doInBackground metodi salju metodi onPostExecute() koja ima zadatak da ta rjesenja posalje main UI thread-u
+ kako bi se on mogao azurirati.
 
 ------------------
 UpdateTaskActivity
 ------------------
+-Osim Main aktivnosti koja se prikazuje pri pokretanju aplikacije kao prvi prozor mi također imamo jos dvije aktivnosti, a to su
+ AddTastActivity i UpdateTaskActivity.
 
 -----
 Task
