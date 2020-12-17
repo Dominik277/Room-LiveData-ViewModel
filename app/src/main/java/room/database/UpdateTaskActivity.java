f2d3428f43193a375d79a0bfc0b8c84d96476ce4
Dominik277 <dominikdoric277@gmail.com>
2020-12-17 15:06:08 +0100

    Dodane reference na XML View-ove tako da smo sada stvorili java objekte tipa View koje mozemo sada prihvacati.

--- app/src/main/java/room/database/UpdateTaskActivity.java
+++ app/src/main/java/room/database/UpdateTaskActivity.java
@@ -1,4 +1,41 @@
 package room.database;
 
-public class UpdateTaskActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UpdateTaskActivity extends AppCompatActivity {
+
+    private TextView editTextTask;
+    private TextView editTextDesc;
+    private TextView editTextFinishBy;
+    private CheckBox checkBoxFinished;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_task);
+
+        editTextTask = findViewById(R.id.editTextTask);
+        editTextDesc = findViewById(R.id.editTextDesc);
+        editTextFinishBy = findViewById(R.id.editTextFinishBy);
+        checkBoxFinished = findViewById(R.id.checkBoxFinished);
+
+        final  Task task = (Task) getIntent().getSerializableExtra("task");
+        loadTask(task);
+
+        findViewById(R.id.button_update).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Clicked",Toast.LENGTH_LONG).show();
+                updateTask(task);
+            }
+        });
+
+    }
 }
