0fb641b39e40665100e78d5efbb0b6d2039ce078
Dominik277 <dominikdoric277@gmail.com>
2020-12-17 16:21:29 +0100

    Dodana updateTask metoda.

--- app/src/main/java/room/database/UpdateTaskActivity.java
+++ app/src/main/java/room/database/UpdateTaskActivity.java
@@ -75,6 +75,52 @@ public class UpdateTaskActivity extends AppCompatActivity {
 
     private void updateTask(final Task task){
 
+        final String sTask = editTextTask.getText().toString().trim();
+        final String sDesc = editTextDesc.getText().toString().trim();
+        final String sFinishBy = editTextFinishBy.getText().toString().trim();
+
+        if (sTask.isEmpty()){
+            editTextTask.setError("Task required");
+            editTextTask.requestFocus();
+            return;
+        }
+
+        if (sDesc.isEmpty()){
+            editTextDesc.setError("Desc required");
+            editTextDesc.requestFocus();
+            return;
+        }
+
+        if (sFinishBy.isEmpty()){
+            editTextFinishBy.setError("Finish by required");
+            editTextFinishBy.requestFocus();
+            return;
+        }
+
+        class UpdateTask extends AsyncTask<Void,Void,Void>{
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                task.setTask(sTask);
+                task.setDesc(sDesc);
+                task.setFinishBy(sFinishBy);
+                task.setFinished(checkBoxFinished.isChecked());
+                DatabaseClient.getInstance(getApplicationContext()).getAppDatabase()
+                        .taskDao()
+                        .update(task);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                Toast.makeText(getApplicationContext(),"Updated",Toast.LENGTH_LONG).show();
+                finish();
+                startActivity(new Intent(UpdateTaskActivity.this,MainMainActivity.class));
+            }
+        }
+        UpdateTask ut = new UpdateTask();
+        ut.execute();
     }
 
     private void deleteTask(final Task task){
