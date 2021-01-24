da699b79c70d0cd2630a760e0086429aafe380b9
Dominik277 <dominikdoric277@gmail.com>
2021-01-24 22:25:49 +0100

    Dodane sve aktivnosti koje ce mi za pocetak trebati.

---
+++ app/src/main/java/room/database/HardverActivity.java
@@ -0,0 +1,14 @@
+package room.database;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HardverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hardver);
+    }
+}
