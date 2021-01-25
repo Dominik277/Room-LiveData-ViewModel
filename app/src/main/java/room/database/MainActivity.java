1d2208fabe40edfe9c1fa3aae95e7fb6a604a62b
Dominik277 <dominikdoric277@gmail.com>
2021-01-25 18:09:18 +0100

    Kreiran navigation drawer.

--- app/src/main/java/room/database/MainActivity.java
+++ app/src/main/java/room/database/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,31 +16,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu,menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.menu_programiranje:
-                startActivity(new Intent(MainActivity.this,ProgramiranjeActivity.class));
-                break;
-            case R.id.menu_hardver:
-                startActivity(new Intent(MainActivity.this,HardverActivity.class));
-                break;
-            case R.id.menu_mrezeiprotokoli:
-                startActivity(new Intent(MainActivity.this,MrezeIProtokoliActivity.class));
-                break;
-            case R.id.menu_operativnisustavi:
-                startActivity(new Intent(MainActivity.this,MrezeIProtokoliActivity.class));
-                break;
-        }
-        return true;
     }
 }
