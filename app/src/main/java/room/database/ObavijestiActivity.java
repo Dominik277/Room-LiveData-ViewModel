2240fc09adbbbf04c017589a35cff5a056a5655d
Dominik277 <dominikdoric277@gmail.com>
2021-01-25 20:24:27 +0100

    Dodane ipak jos dvije aktivnosti.

---
+++ app/src/main/java/room/database/ObavijestiActivity.java
@@ -0,0 +1,14 @@
+package room.database;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ObavijestiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_obavijesti);
+    }
+}
