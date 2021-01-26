d37b7f2212ecf2bb92b08848369eaba754f0d41b
Dominik277 <dominikdoric277@gmail.com>
2021-01-26 12:19:57 +0100

    Dodan vizualni dio za aktivnost unošenja obavijesti.

--- app/src/main/java/room/database/DodajNovuObavijestActivity.java
+++ app/src/main/java/room/database/DodajNovuObavijestActivity.java
@@ -10,5 +10,7 @@ public class DodajNovuObavijestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dodaj_novu_obavijest);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setTitle("Dodaj novu obavijest!");
     }
 }
