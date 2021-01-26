6386971cd6f99795c623f705ac864f2dd346e652
Dominik277 <dominikdoric277@gmail.com>
2021-01-26 11:12:13 +0100

    SplashScreenActivity zavrsen onako kako je zamisljen.

--- app/src/main/java/room/database/SplashScreenActivity.java
+++ app/src/main/java/room/database/SplashScreenActivity.java
@@ -17,17 +17,21 @@ public class SplashScreenActivity extends AppCompatActivity {
                 .withFullScreen()
                 .withTargetActivity(MainActivity.class)
                 .withSplashTimeOut(5000)
-                .withBackgroundColor(Color.parseColor("#1a1b29"))
-                .withHeaderText("Header")
-                .withFooterText("Footer")
-                .withBeforeLogoText("Before Logo Text")
-                .withAfterLogoText("After Logo Text")
+                .withBackgroundResource(R.drawable.farm2)
+                .withHeaderText("Dobrodošli na OPG Dorić,Ovdje možete dobiti osnovne informacije o nama!")
+                .withFooterText("Pridrižite nam se u aplikaciji!")
+                .withBeforeLogoText("OPG DORIĆ")
+                .withAfterLogoText("OPG DORIĆ")
                 .withLogo(R.mipmap.ic_launcher_round);
 
-        config.getHeaderTextView().setTextColor(Color.WHITE);
-        config.getFooterTextView().setTextColor(Color.WHITE);
-        config.getBeforeLogoTextView().setTextColor(Color.WHITE);
-        config.getAfterLogoTextView().setTextColor(Color.WHITE);
+        config.getHeaderTextView().setTextColor(Color.BLACK);
+        config.getFooterTextView().setTextColor(Color.BLACK);
+        config.getBeforeLogoTextView().setTextColor(Color.BLACK);
+        config.getAfterLogoTextView().setTextColor(Color.BLACK);
+        config.getHeaderTextView().setGravity(25);
+        config.getFooterTextView().setTextSize(25);
+        config.getBeforeLogoTextView().setTextSize(30);
+        config.getAfterLogoTextView().setTextSize(30);
 
         View easySplashScreen = config.create();
         setContentView(easySplashScreen);
