44f04ce001437be2ee6a4f155b6bbccc7560ef36
Dominik277 <dominikdoric277@gmail.com>
2021-01-26 10:41:49 +0100

    Dodan SplashScreenActivity u aplikaciju pomocu third party libriry-a EasySplashScreen.

---
+++ app/src/main/java/room/database/SplashScreenActivity.java
@@ -0,0 +1,37 @@
+package room.database;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import gr.net.maroulis.library.EasySplashScreen;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EasySplashScreen config = new EasySplashScreen(SplashScreenActivity.this)
+                .withFullScreen()
+                .withTargetActivity(MainActivity.class)
+                .withSplashTimeOut(50000)
+                .withBackgroundColor(Color.parseColor("#1a1b29"))
+                .withHeaderText("Header")
+                .withFooterText("Footer")
+                .withBeforeLogoText("Before Logo Text")
+                .withAfterLogoText("After Logo Text")
+                .withLogo(R.mipmap.ic_launcher_round);
+
+        config.getHeaderTextView().setTextColor(Color.WHITE);
+        config.getFooterTextView().setTextColor(Color.WHITE);
+        config.getBeforeLogoTextView().setTextColor(Color.WHITE);
+        config.getAfterLogoTextView().setTextColor(Color.WHITE);
+
+        View easySplashScreen = config.create();
+        setContentView(easySplashScreen);
+
+    }
+}
