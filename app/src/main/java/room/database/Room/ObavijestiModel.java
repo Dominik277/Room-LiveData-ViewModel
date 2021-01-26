a2ae5e5d3021c44dd99b498e4f622c87358384c8
Dominik277 <dominikdoric277@gmail.com>
2021-01-26 20:35:39 +0100

    Kreirana jedna entity klasa koja ce predstavljati jednu tablicu unutar baze podataka.

---
+++ app/src/main/java/room/database/Room/ObavijestiModel.java
@@ -0,0 +1,28 @@
+package room.database.Room;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class ObavijestiModel {
+
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    @ColumnInfo(name = "obavijesti")
+    public String obavijest;
+
+    @ColumnInfo(name = "autor")
+    public String autor;
+
+    @ColumnInfo(name = "za_koga")
+    public String zaKoga;
+
+    @ColumnInfo(name = "do_kada")
+    public String doKada;
+
+    @ColumnInfo(name = "objavljeno")
+    public String objavljeno;
+
+}
