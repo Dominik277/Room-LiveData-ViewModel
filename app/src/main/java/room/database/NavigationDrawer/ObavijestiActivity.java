bac30c38e28215dc3ee8f846c6bff5795756c6b6
Dominik277 <dominikdoric277@gmail.com>
2021-01-26 19:18:32 +0100

    Kreirana sva logika za implementaciju CardView-ova unutar RecyclerView, jedino preostalo unijeti podatke koji se trebaju prikazati.

--- app/src/main/java/room/database/NavigationDrawer/ObavijestiActivity.java
+++ app/src/main/java/room/database/NavigationDrawer/ObavijestiActivity.java
@@ -9,14 +9,22 @@ import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import room.database.Adapter;
 import room.database.DodajNovuObavijestActivity;
+import room.database.ModelExampleClass;
 import room.database.R;
 
 public class ObavijestiActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private ArrayList<ModelExampleClass> modelExampleClasses;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,23 @@ public class ObavijestiActivity extends AppCompatActivity {
         setContentView(R.layout.activity_obavijesti);
         setTitle("Obavijesti");
 
+        recyclerView = findViewById(R.id.recyclerView);
+
+        modelExampleClasses = new ArrayList<>();
+        modelExampleClasses.add(new ModelExampleClass("","","","",""));
+        modelExampleClasses.add(new ModelExampleClass("","","","",""));
+        modelExampleClasses.add(new ModelExampleClass("","","","",""));
+        modelExampleClasses.add(new ModelExampleClass("","","","",""));
+        modelExampleClasses.add(new ModelExampleClass("","","","",""));
+        modelExampleClasses.add(new ModelExampleClass("","","","",""));
+
+        Adapter adapter = new Adapter(this,modelExampleClasses);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this
+                ,LinearLayoutManager.VERTICAL
+                ,false);
+
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(adapter);
 
     }
 
