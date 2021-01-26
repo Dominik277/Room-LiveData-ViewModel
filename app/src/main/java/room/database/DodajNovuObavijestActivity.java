65c83ca62d2a347d207782cc54a74bd341ea888e
Dominik277 <dominikdoric277@gmail.com>
2021-01-26 11:56:22 +0100

    Dodana nova aktivnost unutar koje cemo dodavati nove obavijesti i dodano da se otvara na klik action button-a.

---
+++ app/src/main/java/room/database/DodajNovuObavijestActivity.java
@@ -0,0 +1,14 @@
+package room.database;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DodajNovuObavijestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dodaj_novu_obavijest);
+    }
+}
--- app/src/main/java/room/database/NavigationDrawer/ObavijestiActivity.java
+++ app/src/main/java/room/database/NavigationDrawer/ObavijestiActivity.java
@@ -1,12 +1,18 @@
 package room.database.NavigationDrawer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import room.database.DodajNovuObavijestActivity;
 import room.database.R;
 
 public class ObavijestiActivity extends AppCompatActivity {
@@ -19,6 +25,22 @@ public class ObavijestiActivity extends AppCompatActivity {
         setTitle("Obavijesti");
 
 
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_obavijesti,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_obavijest:
+                startActivity(new Intent(ObavijestiActivity.this, DodajNovuObavijestActivity.class));
+                break;
+        }
+        return true;
     }
 }
